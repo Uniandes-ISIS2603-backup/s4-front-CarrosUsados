950981d10ff0ce73991ef01c82694fb6927889c1
jsbravo-sw <js.bravo@uniandes.edu.co>
2018-11-22 11:52:00 -0500

    Corrección submit Administrador y Cliente. Verificación contraseña fuerte en ambos casos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {AdministradorModule} from './administrador/administrador.module';
 import { AppRoutingModule } from './routing-module/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
+import { ModalDialogModule } from 'ngx-modal-dialog';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpErrorInterceptor } from './interceptors/httperrorinterceptor.service';
 import { FichatecnicaModule } from './fichatecnica/fichatecnica.module';
@@ -33,6 +34,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     MarcaModule,
     ArticuloModule,
+    ModalDialogModule.forRoot(),
     AutomovilModule,
     FichatecnicaModule,
     PuntoventaModule,
@@ -40,7 +42,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ClienteModule,
     ModeloModule,
     FormsModule,
-    ToastrModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+  }),
     IngresarModule,
     InicioModule,
     RegistroModule,
